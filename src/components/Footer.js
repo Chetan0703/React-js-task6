db6c73139b7ded392eeca5f3ead9d98ad534d161
Chetan0703 <121075360+Chetan0703@users.noreply.github.com>
2023-11-18 20:27:02 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col> */}
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/mitbhardiwala/"><img src={navIcon1} alt="Icon" /></a>
-              <a href="https://www.facebook.com/meet.bhardiwala.9/"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://www.instagram.com/meet_bhardiwala_0763/"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/chetan-chandak-3a5034232/"><img src={navIcon1} alt="Icon" /></a>
+              <a href=""><img src={navIcon2} alt="Icon" /></a>
+              <a href=""><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
